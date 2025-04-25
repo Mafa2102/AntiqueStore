b42c19d27f0f316267d031209e0ac80a96450bd2
Maria Usina <maryusina@Marias-Air.attlocal.net>
2025-04-25 18:15:38 -0500

    Initial commit - Spring Boot Gradle project

---
+++ src/main/java/com/antique_store/antique_store/AntiqueStoreApplication.java
@@ -0,0 +1,13 @@
+package com.antique_store.antique_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiqueStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiqueStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antique_store/antique_store/AntiqueStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antique_store.antique_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiqueStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
